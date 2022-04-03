c8f45684f64a6e794afcb29d25a781bd6c94cc4e
Alexandre Leuthreau <36959578+alex-nextDev@users.noreply.github.com>
2022-04-03 21:41:43 +0200

    Create DebitException.java

---
+++ src/DebitException.java
@@ -0,0 +1,16 @@
+package com.bankonet;
+
+
+public class DebitException extends CompteException {
+	
+
+		public DebitException(){}
+		
+		public DebitException(String msg)
+		{
+			super(msg);
+			
+		}
+		
+		
+	}
